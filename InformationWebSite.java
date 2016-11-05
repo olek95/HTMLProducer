2e2231d84fd7907fb6c04ccffacc7bfceffa438d
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-05 23:11:01 +0100

    Update InformationWebSite.java
    początek pisania klasy

--- InformationWebSite.java
+++ InformationWebSite.java
@@ -1,5 +1,8 @@
 package htmlproducer;
 
 public class InformationWebSite extends WebSite{
-    
+    public InformationWebSite(){
+        super();
+        titleElement.appendChild(htmlDocument.createTextNode("Strona informacyjna."));
+    }
 }
