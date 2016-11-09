2970ad560a3aea82d30bade2faa3da851bf539f2
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-09 22:21:38 +0100

    Update WebSiteType.java
    dodanie komentarza

--- WebSiteType.java
+++ WebSiteType.java
@@ -1,5 +1,10 @@
 package htmlproducer;
-
+/**
+ * Typ wyliczeniowy <code>WebSiteType</code> reprezentuje typy stron internetowych. 
+ * Możliwe rodzaje to: PHOTOS - album zdjęć, INFORMATION - strona informacyjna, 
+ * CONTACT - strona kontaktowa oraz NEWS - aktualności. 
+ * @author AleksanderSklorz
+ */
 public enum WebSiteType {
     PHOTOS, 
     INFORMATION,
