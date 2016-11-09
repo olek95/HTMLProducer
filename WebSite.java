5554f5f861240fbc34ab84c6ac64e8b87b27e829
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-09 22:22:09 +0100

    Update WebSite.java
    dodanie komentarza, przeniesienie metody tworzącej tekst

--- WebSite.java
+++ WebSite.java
@@ -1,6 +1,7 @@
 package htmlproducer;
 
 import java.io.ByteArrayOutputStream;
+import java.util.Random;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.xml.parsers.DocumentBuilderFactory;
@@ -14,10 +15,17 @@ import javax.xml.transform.stream.StreamResult;
 import org.w3c.dom.Document;
 import org.w3c.dom.Element;
 
-public class WebSite {
-    protected static Document htmlDocument;
+/**
+ * Klasa <code>WebSite</code> reprezentuje stronę internetową w sensie ogólnym. 
+ * Zawiera pola i metody związane z tworzeniem struktury kodu HTML oraz wspomagające 
+ * pisanie kodu klas podrzędnych. 
+ * @author AleksanderSklorz
+ */
+public abstract class WebSite {
+    protected Document htmlDocument;
     protected Element rootElement;
     protected Element titleElement;
+    protected Random rand = new Random(); // dodane w celu optymalizacji, gdyż obiekt ten jest często wykorzystywany i aby nie był ciągle na nowo tworzony
     public WebSite(){
         try{
             htmlDocument = DocumentBuilderFactory.newInstance().newDocumentBuilder().newDocument();
@@ -39,6 +47,12 @@ public class WebSite {
         ByteArrayOutputStream baos = new ByteArrayOutputStream();
         htmlTransformer.transform(new DOMSource(htmlDocument), new StreamResult(baos));
         return baos.toString();
+    }
+    protected String generateText(int numberCharacters, int rangeStart, int textSize){
+        String text = "";
+        for(int i = 0; i < textSize; i++)
+            text += (char)(rand.nextInt(numberCharacters) + rangeStart);
+        return text;
     }
 }
 
