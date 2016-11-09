bd85be3d2a2eb0bec44b084cac68927cded35dbb
AleksanderSklorz <olek1995s@poczta.onet.pl>
2016-11-09 22:23:58 +0100

    Update ContactWebSite.java
    tworzenie strony kontaktowej, usunięcie metody tworzącej tekst i przeniesienie jej do innej klasy

--- ContactWebSite.java
+++ ContactWebSite.java
@@ -1,25 +1,22 @@
 package htmlproducer;
 
-import static htmlproducer.WebSite.htmlDocument;
-import java.util.Random;
 import org.w3c.dom.Element;
-import org.w3c.dom.Text;
 
 public class ContactWebSite extends WebSite{
     public ContactWebSite(){
         super();
         Element bodyElement;
-        titleElement.appendChild(htmlDocument.createTextNode("Galeria zdjęć."));
+        titleElement.appendChild(htmlDocument.createTextNode("Strona kontaktowa."));
         bodyElement = htmlDocument.createElement("body");
         rootElement.appendChild(bodyElement);
         createField("Imię: ", bodyElement);
         createField("Nazwisko: ", bodyElement);
         createField("Miasto: ", bodyElement);
+        createField("Ulica: ", bodyElement);
         createField("Kod pocztowy: ", bodyElement);
         createField("Nr telefonu: ", bodyElement);
     }
     private void createField(String name, Element bodyElement){
-        Random rand = new Random(); 
         Element paragraphElement, boldElement;
         paragraphElement = htmlDocument.createElement("p");
         bodyElement.appendChild(paragraphElement);
@@ -27,14 +24,17 @@ public class ContactWebSite extends WebSite{
         paragraphElement.appendChild(boldElement);
         boldElement.appendChild(htmlDocument.createTextNode(name));
         if(name.equals("Imię: ") || name.equals("Nazwisko: ") || name.equals("Miasto: "))
-            paragraphElement.appendChild(generateText(26, 97, rand.nextInt(20) + 1));
-        else if(name.equals("Kod pocztowy: "))
-    }
-    private Text generateText(int numberCharacters, int rangeStart, int textSize){
-        Random rand = new Random();
-        String text = "";
-        for(int i = 0; i < textSize; i++)
-            text += (char)(rand.nextInt(numberCharacters) + rangeStart);
-        return htmlDocument.createTextNode(text);
+            paragraphElement.appendChild(htmlDocument.createTextNode(generateText(26, 97, rand.nextInt(20) + 1)));
+        else{
+            if(name.equals("Ulica: ")){
+                paragraphElement.appendChild(htmlDocument
+                        .createTextNode(generateText(26, 97, rand.nextInt(20) + 1) + " " + generateText(10, 48, rand.nextInt(2) + 1) + "/" + generateText(10, 48, rand.nextInt(2) + 1)));
+            }else{ 
+                if(name.equals("Kod pocztowy: ")){
+                    paragraphElement.appendChild(htmlDocument.createTextNode(generateText(10, 48, 2) + "-" + generateText(10, 48, 3)));
+                }else paragraphElement.appendChild(htmlDocument.createTextNode(generateText(10, 48, 9)));
+            }
+        }
     }
 }
+
